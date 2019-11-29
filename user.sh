48d2fccb5eaf3452411466edb0ddae2419491d35
root <git.zubur@gmail.com>
2019-11-29 15:43:42 +0200

    bug fix

--- user.sh
+++ user.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-#Auto create user wit default config
+auto create user wit default config
 
 #set -x
 
@@ -24,13 +23,12 @@ egrep -w "^$username" /etc/passwd >/dev/null
 [ $? -eq 0 ] && echo "$username exists!" && exit 1
 
 #Group
-read -p "Choose which group $username will be join to? `echo $'\nPress 1 for "users" or 2 for "sudo"'` " num
-if [[ "$num" == 1 ]] ; then
+read -p "Choose which group $username will be join to? `echo '\nPress 1 for "users" or 2 for "sudo"'` " num
+if [ $num -eq 1 ] ; then
         grp=$USR_GRP
-elif [[ "$num" == 2 ]]; then
+elif [ $num -eq 2 ]; then
         grp="$SUDO_GRP"
 else
         echo "ERROR, unsupported choise"
 fi
-
-sudo useradd -m -G "$grp" -s /bin/bash/ -p $(openssl passwd -1 "$DEF_PASS" ) "$username"  && chgrp "$USR_GRP" /home/"$username"/
+sudo useradd -m -G "$grp" -c "auto Created" -s /bin/bash/ -p $(openssl passwd -1 "$DEF_PASS" ) "$username"  && chgrp "$USR_GRP" /home/"$username"/
