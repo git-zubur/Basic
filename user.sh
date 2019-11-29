a9a3084873d0948536e190080ee6168060e5af7d
root <git.zubur@gmail.com>
2019-11-29 15:55:15 +0200

    fix shell bug

--- user.sh
+++ user.sh
@@ -23,7 +23,7 @@ egrep -w "^$username" /etc/passwd >/dev/null
 [ $? -eq 0 ] && echo "$username exists!" && exit 1
 
 #Group
-read -p "Choose which group $username will be join to? `echo '\nPress 1 for "users" or 2 for "sudo"'` " num
+read -p "Choose which group $username will be join to? `echo $'\nPress 1 for "users" or 2 for "sudo"'` " num
 if [ $num -eq 1 ] ; then
         grp=$USR_GRP
 elif [ $num -eq 2 ]; then
@@ -31,4 +31,4 @@ elif [ $num -eq 2 ]; then
 else
         echo "ERROR, unsupported choise"
 fi
-sudo useradd -m -G "$grp" -c "auto Created" -s /bin/bash/ -p $(openssl passwd -1 "$DEF_PASS" ) "$username"  && chgrp "$USR_GRP" /home/"$username"/
+sudo useradd -m -G "$grp" -c "auto Created" -s /bin/bash -p $(openssl passwd -1 "$DEF_PASS" ) "$username"  && chgrp "$USR_GRP" /home/"$username"/ && chage -d 0 $username
